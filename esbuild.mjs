4587333093aa1bdc3d165adcbfb268feee74baac
Night Star <57443207+Night-Star04@users.noreply.github.com>
2025-01-02 12:53:04 +0800

    refactor(build): modularize build config generation
    - Use `generateBundlerConfig` to streamline configuration setup.
    - Ensure `clearOutput` function awaits the removal operation.
    - Introduce a structured array for build tasks with names and functions.
    - Enhance error handling and logging in the main function.

--- esbuild.mjs
+++ esbuild.mjs
@@ -15,7 +15,7 @@ async function clearOutput() {
     if (!isProductionMode) {
         return;
     }
-    return rm('out', { recursive: true, force: true });
+    await rm('out', { recursive: true, force: true });
 }
 
 /**
@@ -32,22 +32,37 @@ async function performBuildTask(ctx) {
     }
 }
 
+/**
+ * Get the configuration for the build task
+ * @param {string} src - The source files
+ * @param {string} dest - The destination directory or file
+ * @param {boolean} [isFile] - Whether the destination is a file
+ * @returns {import('esbuild').BuildOptions}
+ */
+function generateBundlerConfig(src, dest, isFile = false) {
+    const logLevel = isProductionMode ? 'silent' : 'info';
+
+    const entryPoints = isFile ? [src] : glob.sync(src);
+    if (entryPoints.length === 0) {
+        throw new Error(`No files found at ${src}`);
+    }
+    const outputConfig = isFile ? { outfile: dest } : { outdir: dest };
+
+    return { entryPoints, minify: isProductionMode, ...outputConfig, logLevel };
+}
+
 /** Build the extension */
 async function buildExtension() {
-    const logLevel = isProductionMode ? 'silent' : 'info';
     const ctx = await context({
+        ...generateBundlerConfig('./src/extension.ts', 'out/main.js', true),
         tsconfig: './tsconfig.json',
-        entryPoints: ['./src/extension.ts'],
         bundle: true,
         format: 'cjs',
-        minify: isProductionMode,
         sourcemap: !isProductionMode,
         sourcesContent: false,
         platform: 'node',
         target: 'es2022',
-        outfile: 'out/main.js',
         external: ['vscode', 'sharp'],
-        logLevel,
     });
 
     return performBuildTask(ctx);
@@ -55,52 +70,59 @@ async function buildExtension() {
 
 /** Build the CSS files, used for minifying. */
 async function buildCss() {
-    const cssFiles = glob.sync('./src/css/*.css');
-    if (cssFiles.length === 0) {
-        throw new Error('No CSS files found in ./src/css/');
-    }
-    const ctx = await context({
-        entryPoints: cssFiles,
-        minify: isProductionMode,
-        outdir: 'out/css',
-    });
+    const ctx = await context(generateBundlerConfig('./src/js/*.js', 'out/css'));
 
     return performBuildTask(ctx);
 }
 
 /** Build the JS files, used for minifying. */
 async function buildJs() {
-    const jsFiles = glob.sync('./src/js/*.js');
-    if (jsFiles.length === 0) {
-        throw new Error('No JS files found in ./src/js/');
-    }
-    const ctx = await context({
-        entryPoints: jsFiles,
-        minify: isProductionMode,
-        outdir: 'out/js',
-    });
+    const ctx = await context(generateBundlerConfig('./src/js/*.js', 'out/js'));
 
     return performBuildTask(ctx);
 }
+
+/**
+ * Build tasks
+ *
+ * @type {Array<{ name: string, task: () => Promise<void> }>}
+ */
+const buildTasks = [
+    { name: 'Extension', task: buildExtension },
+    { name: 'CSS', task: buildCss },
+    { name: 'JavaScript', task: buildJs },
+];
 
 /** Main function */
 async function main() {
     try {
         await clearOutput();
-        const results = await Promise.allSettled([buildExtension(), buildCss(), buildJs()]);
+        const results = await Promise.allSettled(buildTasks.map((task) => task.task()));
+
+        let isErrorPresent = false;
         results.forEach((result, index) => {
-            if (result.status === 'rejected') {
-                console.error(`✘ [ERROR] Build task ${index + 1} failed`);
+            const taskName = buildTasks[index].name;
+            if (result.status === 'fulfilled') {
+                console.log(`✔ [SUCCESS] Build completed for ${taskName}`);
+            } else {
+                isErrorPresent = true;
+                console.error(`✘ [ERROR] Build failed for ${taskName}`);
                 console.error(result.reason);
             }
         });
 
-        if (!isWatchMode) {
-            console.log('✔ [SUCCESS] Build completed successfully');
+        if (isWatchMode) {
+            console.log('Watching for changes...');
         }
+        if (isErrorPresent) {
+            throw new Error('One or more build tasks failed');
+        }
+
+        console.log('✔ [SUCCESS] Build completed successfully');
     } catch (err) {
         console.error('✘ [ERROR] Build failed');
         console.error(err);
+        process.exit(1);
     }
 }
 
